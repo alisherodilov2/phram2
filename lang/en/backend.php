d3b3f38e09eeacb9f0c5d2b7da6d0224b2d17708
Alisher <alisherodilov741@gmail.com>
2023-12-09 11:01:42 +0500

    new

--- lang/en/backend.php
+++ lang/en/backend.php
@@ -59,55 +59,12 @@ return [
     'about'=>'About us',
     'history'=>'Our history',
     'aboutdes'=>"
-    The private enterprise company \" DORA LINE\" was founded in 2014 and is one of the dynamically developing
-     companies in the pharmaceutical market of Uzbekistan.
-     \"DORA LINE\" is a pharmaceutical company specializing in the production of vitamins, minerals,
-     natural preparations from medicinal herbs and to increase vitality and strengthen
-     health.
-     Currently, the assortment of the DORA LINE company includes 9 types of drugs in
-     tablet form.
-     All \"DORA LINE\" products are made on the basis of natural plant raw materials
-     and mineral origin.
-     The line of natural preparations \"DORA LINE\" is constantly growing due to the development of original
-     drugs with pronounced clinical effectiveness.
-     All equipment and production processes in the DORA LINE company fully comply
-     international standards.
-     In the production of its preparations, “DORA LINE” widely uses substances with great healing power.
-     medicinal herbs growing in the mountains and valleys of Uzbekistan and Russia. Up to 80% used in
-     Our company's herbs are collected in the natural environment of the foothills, known for their environmental friendliness.
-     Our mission - We want to research, develop and bring innovative products to market in a timely manner
-     natural preparations intended for the prevention of diseases, as well as for the relief
-     suffering and improving the quality of life of patients.
-     We also want to provide returns to shareholders that reflect our successful results.
-     activities of \"DORA LINE\", and at the same time reward those who invest their ideas and time in
-     our company.
+    <b> The company \"DORA LINE\"  </b> LLC was founded in 2014 and is one of the dynamically developing companies in the pharmaceutical market of Uzbekistan. \"DORA LINE\" is a pharmaceutical company specializing in the production of vitamins, minerals, natural preparations from medicinal herbs and to increase vitality and improve health. <br><br>
+     <b>Currently, the DORA LINE </b> range of products includes 9 types of drugs in tablet form. All DORA LINE products are made on the basis of natural raw materials of plant and mineral origin. The line of natural drugs \"DORA LINE\" is constantly growing due to the development of original drugs with pronounced clinical effectiveness. All equipment and production processes in the DORA LINE company fully comply with international standards. In the production of its preparations, DORA LINE widely uses medicinal herbs that have great healing power, growing in the mountains and valleys of Uzbekistan and Russia. Up to 80% of the herbs used in our company are collected in the natural environment of the foothills, known for their environmental friendliness. <br><br>
+    <b> Our Mission </b> - We want to research, develop and timely bring to market innovative natural products designed to prevent disease, as well as alleviate suffering and improve the quality of life of patients. We also want to provide returns to shareholders that reflect DORA LINE's successful performance, while at the same time rewarding those who invest their ideas and time in our company. Our main goal is to produce high-quality, effective and affordable drugs. Our goal is to guarantee the high quality and safety of our products. <br><br>
+   <b> DORA LINE preparations </b> are manufactured in accordance with the standard. Each stage of production, from the purchase of raw materials to packaging, as well as storage of finished products, is under the mandatory control of our specialists. The marketing service constantly studies market needs and develops solutions that take into account the needs of doctors and patients. This approach allows us to produce modern drugs at an affordable price and quality that meets international standards. DORA LINE chooses a growth path that takes into account the current and future needs of the pharmaceutical market. We build our future on a responsible attitude towards consumers and healthcare professionals. <br><br>
+    <b>The main value </b> of our company is our employees, so concern for their health and safety is at the heart of all processes. The long-term development strategy of \"DORA LINE\" is based on our verified and diversified portfolio, and its implementation - providing healthcare needs with highly effective medical solutions on a global scale and across all business divisions - is based on three key priorities of the company: - strengthening leadership in innovation through research and development of new drugs and expanding the scope of the existing portfolio; - accelerating growth in all divisions by introducing new drugs to the market and expanding presence in emerging markets; - increasing productivity through a number of corporate initiatives that free up financial resources for investment in research and development of new drugs. By pursuing these priorities, we support the company's growth above the market, deliver returns to investors, and enhance products and patient services through our innovative solutions.
 
-     Our main goal is to produce high-quality, effective and affordable drugs.
-     Our goal is to guarantee the high quality and safety of our products.
-     DORA LINE preparations are manufactured in accordance with the standard. Each stage of production
-     starting from the purchase of raw materials and ending with packaging, as well as storage of finished products is located
-     under the mandatory supervision of our specialists.
-     The marketing service constantly studies market needs and develops solutions that
-     take into account the needs of doctors and patients. This approach makes it possible to produce modern
-     drugs at an affordable price and quality that meets international standards.
-     DORA LINE chooses a growth path that takes into account current and future needs
-     pharmaceutical market. We build our future on a responsible attitude towards consumers and
-     healthcare professionals.
-     The main value of our company is our employees, therefore taking care of their health and
-     Security is at the heart of the organization of all processes.
-     The long-term development strategy of \"DORA LINE\" is based on our proven and
-     diversified portfolio, and its implementation is to meet healthcare needs
-     highly effective medical solutions on a global scale and across all business divisions, -
-     comes from three key company priorities:
-     - strengthening leadership in innovation through research and development of new drugs and
-     expanding the scope of application of the current portfolio;
-     - acceleration of growth in all divisions due to the introduction of new drugs to the market and expansion
-     presence in emerging markets;
-     - increased productivity through a number of corporate initiatives that free up financial
-     resources to invest in research and development of new drugs.
-     By following these priorities, we support the company's growth above the market and ensure profitability
-     investors and improve the quality of products and services for patients thanks to our innovative
-     decisions.
     ",
     'infofotter'=>"Tashkent city, Yunusabad district, Niyozbek road street, 1 house",
     'doraline'=>'"DORA LINE" LLC',
