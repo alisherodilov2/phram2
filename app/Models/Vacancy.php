79679c27b83c5d4a6ef9e46b3900111cfd63aede
alisherodilov2 <alisherodilov741@gmail.com>
2025-01-10 09:33:15 +0500

    changes maion

--- app/Models/Vacancy.php
+++ app/Models/Vacancy.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Vacancy extends Model implements HasMedia
 {
     use HasFactory;
+    use SoftDeletes;
     use InteractsWithMedia;
     protected $fillable = [
         'title',
